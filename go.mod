0328f4b0995156b6cc04a75df356981524e5fecf
vdios <>
2021-03-06 12:49:40 +0600

    connected to postgresql db

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.15
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.9.0
 	github.com/sirupsen/logrus v1.8.0
 	github.com/stretchr/testify v1.7.0
 )
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -5,12 +5,14 @@ import(
   "github.com/gorilla/mux"
   "net/http"
   "io"
+  "github.com/vdios/http-rest-api/internal/app/store"
 )
 
 type APIServer struct{
   config *Config
   logger *logrus.Logger
   router *mux.Router
+  store *store.Store
 }
 
 func New(config *Config) *APIServer{
@@ -21,6 +23,7 @@ func New(config *Config) *APIServer{
   }
 }
 
+//Start
 func (s *APIServer) Start() error{
   if err := s.configureLogger(); err != nil{
     return err
@@ -28,11 +31,15 @@ func (s *APIServer) Start() error{
 
   s.configureRouter()
 
-  s.logger.Info("starting api server")
+  if err:=s.configureStore(); err!=nil{
+    return err
+  }
 
+  s.logger.Info("starting api server")
   return http.ListenAndServe(s.config.BindAddr,s.router)
 }
 
+// configure router
 func (s *APIServer) configureRouter(){
   s.router.HandleFunc("/hello",s.handleHello())
 }
@@ -43,12 +50,23 @@ func (s *APIServer) handleHello() http.HandlerFunc{
   }
 }
 
+//configure logger
 func (s *APIServer) configureLogger() error{
   level,err := logrus.ParseLevel(s.config.LogLevel)
   if err != nil{
     return err
   }
-
   s.logger.SetLevel(level)
+  return nil
+}
+
+//configure store
+func (s *APIServer) configureStore() error{
+  st := store.New(s.config.Store)
+  if err:=st.Open(); err!=nil{
+    return err
+  }
+
+  s.store = st
   return nil
 }
